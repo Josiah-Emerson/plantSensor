7921fdab69878817681d154b3ca4905bda82f5df
Josiah-Emerson <jemerson20@apu.edu>
2025-03-26 16:05:49 -0700

    Basic program for monitoring moisture

---
+++ M5BasicCoreProgram/M5BasicCoreProgram.ino
@@ -0,0 +1,22 @@
+#include <M5Stack.h>
+
+// pin defines
+#define SENSOR_PIN 35
+#define PUMP_PIN 17
+
+// global variables
+int rawMoistureADC; // analog input for moisture
+
+
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rawMoistureADC = analogRead(SENSOR_PIN);
+  Serial.write(rawMoistureADC);
+}
