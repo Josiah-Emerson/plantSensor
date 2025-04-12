e08d27610f3d1f712bbf9acb9e2b8cb88a4ec66e
Josiah-Emerson <jemerson20@apu.edu>
2025-04-11 18:10:03 -0700

    Sends measurement to blynk when button B is pressed

--- M5BasicCoreProgram/M5BasicCoreProgram.ino
+++ M5BasicCoreProgram/M5BasicCoreProgram.ino
@@ -20,24 +20,53 @@ BlynkTimer timer;
 int rawMoistureADC; // analog input for moisture
 
 // function declarations
-void my_timer();
+void sendMoisture();
+void moisturAndPumpSetup();
 
 void setup() {
   M5.begin();
   Serial.begin(9600);
   delay(25);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  timer.setInterval(5000L, my_timer);
+  timer.setInterval(5000L, sendMoisture);
 
-  // setup moisture
-  pinMode(SENSOR_PIN, INPUT);
-  pinMode(PUMP_PIN, OUTPUT);
+  moistureAndPumpSetup();
+  
 }
 
+
+
 void loop() {
-  rawMoistureADC = analogRead(SENSOR_PIN);
-  Blynk.run();
-  timer.run();
+  M5.update();
+  Blynk.run(); // Blynk should run constantly, but timer only in if for sending measurements
+  //TODO - Rename timer to something better
+
+  
+  if(M5.BtnA.wasPressed()){
+    Serial.println("Button A pressed");
+  }
+  
+  if(M5.BtnB.wasPressed()){
+    Serial.println("Button pressed!");
+
+    rawMoistureADC = analogRead(SENSOR_PIN);
+    Serial.print("Current reading: ");
+    Serial.println(rawMoistureADC);
+    delay(25);
+
+    Serial.println("* * * * * SENDING DATA * * * * * ");
+    
+    timer.run();
+    
+    Serial.println("* * * * * DONE | DELAYING * * * * * ");
+    delay(5000);
+  }
+
+}
+
+void moistureAndPumpSetup(){
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(PUMP_PIN, OUTPUT);
 }
 
 BLYNK_WRITE(V1){
@@ -50,7 +79,7 @@ BLYNK_WRITE(V1){
   }
 }
 
-void my_timer(){
+void sendMoisture(){
   Blynk.virtualWrite(0, rawMoistureADC);
   Serial.println("\t* * * sent data to Blynk cloud");
 }
