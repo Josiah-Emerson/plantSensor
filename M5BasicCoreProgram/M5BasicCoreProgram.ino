fdf5fc5ee00ee51b4edaf0fa3fbf0999c8e3bdab
Josiah-Emerson <jemerson20@apu.edu>
2025-04-16 00:16:00 -0700

    Switched from direct esp deep sleep to M5.Power.deepSleep()

--- M5BasicCoreProgram/M5BasicCoreProgram.ino
+++ M5BasicCoreProgram/M5BasicCoreProgram.ino
@@ -20,8 +20,6 @@ BlynkTimer timer;
 #define SENSOR_PIN 35
 #define PUMP_PIN 17
 
-// must be in microseconds... 1 second = 1e6 us
-#define DEEP_SLEEP_DURATION 5e6
 
 int rawMoistureADC; // analog input for moisture
 // The value was mostly around 1660-1680 when first bought
@@ -34,94 +32,44 @@ bool botanistMode;
 void takeMeasurements();
 void sendMeasurements();
 void pump();
+void botanistModeSetup();
 
 
 
 // * * * * * SETUP * * * * * 
 
-void setup() {
+void setup(){
   delay(1000);
   M5.begin();
+  M5.Power.begin();
+  M5.Lcd.setTextSize(2);
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Serial.println();
+  Serial.println("*  *  *  CONNECTED TO WIFI *  *  *");
+  delay(2500);
 
-
-  // *  *  *  *  *  S L E E P  S E T U P  *  *  *  *  *  *
-  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
-  if(wakeup_reason == ESP_SLEEP_WAKEUP_EXT0){
-    Serial.println("Woke up by button");
-    M5.Lcd.println("Woke up by button");
-  }else if(wakeup_reason == ESP_SLEEP_WAKEUP_TIMER){
-    Serial.println("Woke up by timer");
-    M5.Lcd.println("Woke up by timer");
-  }
   
   //  *  *  *  *  * M A I N  F U N C I O N A L I T Y  *  *  *  *  *
+  botanistModeSetup();
   takeMeasurements();
   sendMeasurements();
   pump();
-  //botanistModeSetup();
   
 
   // *  *  *  *  *  P U T  T O  S L E E P  *  *  *  *  *
   Serial.println("Going to sleep...");
   M5.Lcd.println("Going to sleep...");
   delay(2500);
-  esp_sleep_enable_timer_wakeup(DEEP_SLEEP_DURATION);
-  esp_sleep_enable_ext0_wakeup((gpio_num_t) BUTTON_A_PIN, 0);
-  esp_deep_sleep_start();
+  uint64_t sleepTime = 3600e6; // in micro seconds
+  M5.Power.setWakeupButton(BUTTON_A_PIN);
+  M5.Power.deepSleep(sleepTime);
 }
 
 
-// * * * * * LOOP * * * * * 
 
 void loop() {
-  M5.update();
-  Blynk.run(); // Blynk should run constantly, but timer only in if for sending measurements
-  //TODO - Rename timer to something better
-
-  
-  if(M5.BtnA.wasPressed()){
-    Serial.println("Button A pressed");
-  }
-  
-  if(M5.BtnB.wasPressed()){
-    Serial.println("Button pressed!");
-
-    rawMoistureADC = analogRead(SENSOR_PIN);
-    Serial.print("Current reading: ");
-    Serial.println(rawMoistureADC);
-    delay(25);
-
-    Serial.println("* * * * * SENDING DATA * * * * * ");
-    
-    timer.run();
-    
-    Serial.println("* * * * * DONE | DELAYING * * * * * ");
-    delay(5000);
-  }
-
-  Serial.print("Botanist Mode: ");
-  Serial.println(botanistMode);
-
-  rawMoistureADC = analogRead(SENSOR_PIN);
- 
-
-  if(rawMoistureADC > moistureThreshold){ 
-    // Too dry (Remember higher val is drier from experiments)
-    Serial.println("Soil is too dry");   
-    if(!botanistMode){
-      Serial.println("Pumping for 5 sec... ");
-      digitalWrite(PUMP_PIN, true);
-      delay(5000);
-      digitalWrite(PUMP_PIN, false);
-    }else{
-      Serial.println("Not pumping (in botanist mode)");
-    }
-  }
-
-  Serial.println("* * * * * * * * * * * * * * * * * * * * * * * * * *");
-  delay(2500);
+ // Nothing in loop because of deep sleep
 }
 
 
@@ -138,7 +86,6 @@ void takeMeasurements(){
   Serial.println(rawMoistureADC);
 
   // *  *  *  *  *  B A T T E R Y *  *  *  *  *
-  M5.Power.begin();
   batteryLevel = M5.Power.getBatteryLevel();
   Serial.print("Battery: ");
   Serial.println(batteryLevel);
@@ -160,6 +107,11 @@ void pump(){
     // higher value = drier
     Serial.println("Soil too dry");
     M5.Lcd.println("Soil too dry");
+    if(botanistMode){
+      M5.Lcd.println("In BotanistMode | No Action Taken");
+      Serial.println("In BotanistMode | No Action Taken");
+      return;
+    }
     
     Serial.println("* * * 5 SEC PUMP * * *");
     M5.Lcd.println("* * * 5 SEC PUMP * * *");
@@ -173,9 +125,9 @@ void pump(){
 
 }
 void botanistModeSetup(){
-  botanistMode = false;
-  Blynk.virtualWrite(1, 0);
-  // BLYNK_READ (similar to BLYNK_WRITE)
+  Blynk.syncVirtual(V1);
+  Serial.print("Botanist mode: ");
+  Serial.println(botanistMode);
 }
 
 BLYNK_WRITE(V1){
@@ -185,8 +137,10 @@ BLYNK_WRITE(V1){
   }else{
     // Botanist Mode OFF
     botanistMode = false;
-  }
+  }}
 
-}
+  BLYNK_CONNECTED(){
+    Blynk.syncAll();
+  }
 
 
