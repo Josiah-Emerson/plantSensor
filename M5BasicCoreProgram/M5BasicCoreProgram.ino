ada8f69d8114ba36b9592f4b1dbb9cd48e1a3f41
Josiah-Emerson <jemerson20@apu.edu>
2025-04-20 22:37:17 -0700

    NPK capability added

--- M5BasicCoreProgram/M5BasicCoreProgram.ino
+++ M5BasicCoreProgram/M5BasicCoreProgram.ino
@@ -8,8 +8,23 @@
 #include <WiFiClientSecure.h>
 #include <BlynkSimpleEsp32_SSL.h>
 
+/* 
+// APU BYOD
 char ssid[] = "APU-BYOD";
 char pass[] = "chaffy-intertwining-NyAC";
+*/
+
+
+//home
+char ssid[] = "MySpectrumWiFi24-2G";
+char pass[] = "ancientbike885";
+
+
+/*
+//hotspot
+char ssid[] = "Josiah Phone";
+char pass[] = "josiahE1";
+*/
 
 BlynkTimer timer;
 
@@ -18,7 +33,9 @@ BlynkTimer timer;
 
 
 #define SENSOR_PIN 35
-#define PUMP_PIN 17
+#define PUMP_PIN 26
+#define RXD 16
+#define TXD 17
 
 
 int rawMoistureADC; // analog input for moisture
@@ -27,12 +44,18 @@ int rawMoistureADC; // analog input for moisture
 const int moistureThreshold = 1800; // NOTE: lower means more moisture
 int8_t batteryLevel;
 bool botanistMode;
+const uint8_t npk_inq[] = {0x01, 0x03, 0x00, 0x1E, 0x00, 0x03, 0x65, 0xCD};
+uint8_t values[11];
+int nitrogen;
+int phosphorus;
+int potassium;
 
 // function declarations
 void takeMeasurements();
 void sendMeasurements();
 void pump();
 void botanistModeSetup();
+void npk();
 
 
 
@@ -44,6 +67,7 @@ void setup(){
   M5.Power.begin();
   M5.Lcd.setTextSize(2);
   Serial.begin(9600);
+  Serial.println("Connecting to wifi");
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   Serial.println();
   Serial.println("*  *  *  CONNECTED TO WIFI *  *  *");
@@ -58,6 +82,7 @@ void setup(){
   
 
   // *  *  *  *  *  P U T  T O  S L E E P  *  *  *  *  *
+  
   Serial.println("Going to sleep...");
   M5.Lcd.println("Going to sleep...");
   delay(2500);
@@ -78,12 +103,30 @@ void loop() {
 
 void takeMeasurements(){
   // *  *  *  *  *  M O I S T U R E  &  P U M P  *  *  *  *  *
+  pinMode(SENSOR_PIN, HIGH);
   pinMode(SENSOR_PIN, INPUT);
   pinMode(PUMP_PIN, OUTPUT);
   
   rawMoistureADC = analogRead(SENSOR_PIN);
   Serial.print("Moisture: ");
   Serial.println(rawMoistureADC);
+
+  // *  *  *  *  *  N P K *  *  *  *  *
+  Serial2.begin(9600, SERIAL_8N1, RXD, TXD);
+  int nitrogenSum = 0;
+  int phosphorusSum = 0;
+  int potassiumSum = 0;
+  // best practice is to average readings from soil according to manual
+  for (int i = 0; i < 5; i++){
+    npk();
+    nitrogenSum += values[4];
+    phosphorusSum += values[6];
+    potassiumSum += values[8];
+  }
+
+  nitrogen = nitrogenSum/5;
+  phosphorus = phosphorusSum/5;
+  potassium = potassiumSum/5;
 
   // *  *  *  *  *  B A T T E R Y *  *  *  *  *
   batteryLevel = M5.Power.getBatteryLevel();
@@ -97,6 +140,11 @@ void sendMeasurements(){
 
   // send battery level
   Blynk.virtualWrite(2, batteryLevel);
+
+  // send NPK
+  Blynk.virtualWrite(3, nitrogen);
+  Blynk.virtualWrite(4, phosphorus);
+  Blynk.virtualWrite(5, potassium);
 
   M5.Lcd.println("* * * Data sent to Blynk * * *");
   Serial.println("* * * Data sent to Blynk * * *");
@@ -123,6 +171,15 @@ void pump(){
     M5.Lcd.println("Soil is fine");
   }
 
+}
+
+void npk(){
+  if(Serial2.write(npk_inq, sizeof(npk_inq)) == 8){
+    delay(100);
+    for(int i = 0; i < 11; i++){
+      values[i] = Serial2.read();
+    }
+  }
 }
 void botanistModeSetup(){
   Blynk.syncVirtual(V1);
